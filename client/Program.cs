2d5a9781ac91aa3949cd0b6704638283900fd22b
karen <karen.shirvanyan@gmail.com>
2018-11-02 00:19:58 +0400

    init

---
+++ client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using StackExchange.Redis;
+
+namespace client
+{
+    class Program
+    {
+        static ConnectionMultiplexer RedisClient = ConnectionMultiplexer.Connect("localhost:6378");
+        static IDatabase RedisDB;
+        static void Main(string[] args)
+        {
+	    while(true)
+	    {
+		Thread.Sleep(200);
+		try
+		{
+	           var RedisDB = RedisClient.GetDatabase();
+		    var value = RedisDB.StringGet("g");
+	     	    Console.Write(value);
+		}
+		catch(Exception exc)
+		{
+			continue;
+		}
+	    }
+        }
+    }
+}
